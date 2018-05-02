e9d9496aa9879b023db87de6b0c1cd3c9b0757cb
mattheuslee <mattheus.lee@gmail.com>
2018-05-03 01:34:13 +0800

    Comment out to use Arduino mock instead for travis

--- ArduinoLog.h
+++ ArduinoLog.h
@@ -16,11 +16,7 @@ Licensed under the MIT License <http://opensource.org/licenses/MIT>.
 #define LOGGING_H
 #include <inttypes.h>
 #include <stdarg.h>
-#if defined(ARDUINO) && ARDUINO >= 100
-	#include "Arduino.h"
-#else
-	#include "WProgram.h"
-#endif
+
 typedef void (*printfunction)(Print*);
 
 //#include <stdint.h>
